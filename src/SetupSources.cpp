3572672b0afec156e84ce6f2ee90dfdaac92d695
Porta <portalmaster137@gmail.com>
2025-11-02 14:01:54 +1000

    bhh

--- src/SetupSources.cpp
+++ src/SetupSources.cpp
@@ -13,9 +13,11 @@ bool setup_sources() {
     std::string lfs = std::string(LFS);
     std::string lfsurl = std::string(LFSPACKAGELINK);
     std::string lfs_sources = lfs + "/sources";
+    std::string lfs_package_tar = lfs_sources + "/lfs-packages.tar";
     run_cmd_with_prefix("mkdir -v " + lfs_sources, "[Sources]");
     run_cmd_with_prefix("chmod -v a+wt " + lfs_sources, "[Sources]");
-    run_cmd_with_prefix("curl " + lfsurl + " -o " + lfs_sources + "/lfs-packages.tar", "[Sources]");
+    run_cmd_with_prefix("curl " + lfsurl + " -o " + lfs_package_tar, "[Sources]");
+    run_cmd_with_prefix("tar -xpvf " + lfs_package_tar);
 
     return true;
 }
