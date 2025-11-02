02c6b232ce1abefd4757587e299b31db0d2459f6
Porta <portalmaster137@gmail.com>
2025-11-02 14:10:12 +1000

    wippeeeee

--- src/SetupSources.cpp
+++ src/SetupSources.cpp
@@ -17,7 +17,7 @@ bool setup_sources() {
     run_cmd_with_prefix("mkdir -v " + lfs_sources, "[Sources]");
     run_cmd_with_prefix("chmod -v a+wt " + lfs_sources, "[Sources]");
     run_cmd_with_prefix("curl " + lfsurl + " -o " + lfs_package_tar, "[Sources]");
-    run_cmd_with_prefix("tar -xpvf " + lfs_package_tar);
+    run_command("tar -xpvf " + lfs_package_tar, lfs_sources, "[Sources]");
 
     return true;
 }
